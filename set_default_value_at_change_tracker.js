3fa91c53386de5d382c4ed4f56af3b5d486d5402
onozaty <onozaty@gmail.com>
2015-11-25 01:07:06 +0900

    デフォルト値を設定する例を追加。(担当者も)

--- set_default_value_at_change_tracker.js
+++ set_default_value_at_change_tracker.js
@@ -12,18 +12,20 @@ $(function() {
 
   var setDefalutValue = function() {
     // トラッカーに応じてデフォルト値を設定
-    // 例: 予定工数を設定
+    // 例: 予定工数、担当者を設定
     switch($('#issue_tracker_id').val()) {
       case '1':
-        $('#issue_estimated_hours').val(2);
+        $('#issue_estimated_hours').val('2');
         break;
 
       case '2':
-        $('#issue_estimated_hours').val(4);
+        $('#issue_estimated_hours').val('4');
+        $('#issue_assigned_to_id').val('1');
         break;
 
       case '3':
-        $('#issue_estimated_hours').val(1);
+        $('#issue_estimated_hours').val('1');
+        $('#issue_assigned_to_id').val('2');
         break;
     }
   }
