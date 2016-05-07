c4ca619f1e9dab11d7d79b4dcce982cf62f66f5a
onozaty <onozaty@gmail.com>
2016-05-07 11:14:25 +0900

    ステータス変更時などのDOM再構築時に動作しなくなる問題対処

--- add_me_for_custome_users_field.js
+++ add_me_for_custome_users_field.js
@@ -6,15 +6,29 @@ $(function() {
   var userUrl = $("div#loggedas a").attr("href");
   var userId = userUrl.substr(userUrl.lastIndexOf('/') + 1);
 
-  // ユーザ一覧のカスタムフィールドを取得
-  $('select.user_cf').each(function() {
-    // 自分自身の選択肢を取得
-    var meOption = $(this).find('option[value="' + userId + '"]').first();
+  var appendMe = function() {
+    // ユーザ一覧のカスタムフィールドを取得
+    $('select.user_cf').each(function() {
+      // 自分自身の選択肢を取得
+      var meOption = $(this).find('option[value="' + userId + '"]').first();
 
-    // 複製し、空項目の次に名前を変えて挿入
-    meOption.clone()
-      .removeAttr('selected') // 2重に選択となってしまうので
-      .insertAfter($(this).find('option[value=""]'))
-      .text('<<自分>>');
-  });
-})
+      // 複製し、空項目の次に名前を変えて挿入
+      meOption.clone()
+        .removeAttr('selected') // 2重に選択となってしまうので
+        .insertAfter($(this).find('option[value=""]'))
+        .text('<<me>>');
+    });
+  }
+
+  appendMe();
+
+  // ステータス変更時などにDOMが差し替えられるので
+  // フォームの内容が書き変わるたびに絞り込みを反映
+  var _replaceIssueFormWith = replaceIssueFormWith;
+  replaceIssueFormWith = function(html){
+
+    _replaceIssueFormWith(html);
+
+    appendMe();
+  };
+});
