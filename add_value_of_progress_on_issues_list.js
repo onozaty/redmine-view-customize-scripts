1e82d8b0309cfec66f32df6de1a02e506c185ba2
onozaty <onozaty@gmail.com>
2016-09-04 01:44:16 +0900

    チケット一覧の進捗欄で進捗の値を表示するサンプル作成

---
+++ add_value_of_progress_on_issues_list.js
@@ -0,0 +1,21 @@
+/*
+Path pattern: /issues?
+Type: JavaScript
+
+チケット一覧の進捗欄で進捗の値を表示
+*/
+$(function() {
+  $('table.progress').each(function() {
+    var target = $(this);
+
+    // classから進捗の値を取得(progress-値 となっている)
+    var match = target.attr('class').match(/progress-([0-9]+)/);
+    if (!match) {
+      return;
+    }
+
+    // 値を追記して表示
+    var value = match[1];
+    target.after($('<p>').text(value));
+  });
+});
