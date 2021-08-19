17124cff5c44e9d9882808de32025615133d3d46
onozaty <onozaty@gmail.com>
2021-08-19 23:04:47 +0900

    v1.2.0以降の書き方でシンプルに

--- set_default_value_at_change_tracker.js
+++ set_default_value_at_change_tracker.js
@@ -1,35 +1,23 @@
 // チケット作成時にトラッカーに応じてデフォルト値を設定
 //
-// Path pattern: /issues/new$
+// Insertion position: Bottom of issue form
 // Type: JavaScript
 $(function() {
-  $('#all_attributes').change(function(e) {
-    if (e.target.id == 'issue_tracker_id') {
-      // トラッカーIDが変わった場合に初期値を切り替え
-      setDefalutValue();
-    }
-  });
+  // トラッカーに応じてデフォルト値を設定
+  // 例: 予定工数、担当者を設定
+  switch($('#issue_tracker_id').val()) {
+    case '1':
+      $('#issue_estimated_hours').val('2');
+      break;
 
-  var setDefalutValue = function() {
-    // トラッカーに応じてデフォルト値を設定
-    // 例: 予定工数、担当者を設定
-    switch($('#issue_tracker_id').val()) {
-      case '1':
-        $('#issue_estimated_hours').val('2');
-        break;
+    case '2':
+      $('#issue_estimated_hours').val('4');
+      $('#issue_assigned_to_id').val('1');
+      break;
 
-      case '2':
-        $('#issue_estimated_hours').val('4');
-        $('#issue_assigned_to_id').val('1');
-        break;
-
-      case '3':
-        $('#issue_estimated_hours').val('1');
-        $('#issue_assigned_to_id').val('2');
-        break;
-    }
+    case '3':
+      $('#issue_estimated_hours').val('1');
+      $('#issue_assigned_to_id').val('2');
+      break;
   }
-
-  // 現在選択されているものをもとに
-  setDefalutValue();
 });
